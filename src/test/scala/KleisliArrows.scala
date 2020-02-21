051b84cd34b213fcbf4f47dcebc099f5ca49581f
Jacob Xie <jacobbishopxy@gmail.com>
2020-02-21 17:01:54 +0800

    test cast: update KleisliArrows.scala

--- src/test/scala/KleisliArrows.scala
+++ src/test/scala/KleisliArrows.scala
@@ -20,7 +20,6 @@ object KleisliArrows extends App {
 
   println(s"x0: ${pipeline(3)}") // "18"
 
-
   /**
    * 那么遇到Monadic函数会是什么样的情况呢？
    */
@@ -36,7 +35,6 @@ object KleisliArrows extends App {
    * 函数衔接部分的类型不一样，都不用编译我们就知道这不可能组合起来。
    * 如果不用flatMap我们就要对第一个函数的返回值手动解包，判断情况再输入到第二个函数得出结果。
    */
-
 
   val x1 = Option("1000").flatMap(stringToNonEmptyString).flatMap(stringToNumber)
   println(s"x1: $x1")
@@ -60,7 +58,18 @@ object KleisliArrows extends App {
   println(s"x4: $x4")
 
   /**
-   * 拓展一下，我们试试Kleisli这个由Cats实现的数据类型（Kleisli Category）
+   * 拓展一下，如何运用for表达式进行sequencing computation。
+   * 关于sequencing computation是否可以理解为有依赖的连续计算？
+   */
+
+  val x5 = for {
+    x <- stringToNonEmptyString("1000")
+    y <- stringToNumber(x)
+  } yield y
+  println(s"x5: $x5")
+
+  /**
+   * 再拓展一下，我们试试Kleisli这个由Cats实现的数据类型（Kleisli Category）
    */
 
   import cats.data.Kleisli
@@ -70,8 +79,8 @@ object KleisliArrows extends App {
 
   val pipelineK: Kleisli[Option, String, Int] = stringToNumberK compose stringToNonEmptyStringK
 
-  println(s"x5: ${pipelineK("1000")}")
-  println(s"x6: ${pipelineK("Sam")}")
+  println(s"x6: ${pipelineK("1000")}")
+  println(s"x7: ${pipelineK("Sam")}")
 
 }
 
